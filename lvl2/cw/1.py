5208014ec569abcf81211a4242a78899bc9f9b40
giorgavar <itzblacklord@gmail.com>
2025-10-10 20:12:54 +0400

    third commit

---
+++ lvl2/cw/1.py
@@ -0,0 +1,13 @@
+print('Gio')
+print(9)
+print(10.2)
+
+name = 'Gio'
+age = 13
+favorite,food = "Kitri"
+favoritecolor = 'black'
+
+print(name)
+print(age)
+print(favorite,food)
+print(favoritecolor)
